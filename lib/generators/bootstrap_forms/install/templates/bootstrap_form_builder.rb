b3896f83020705e62b16981a415b886fdf11451c
Joel Cogen <joel@joelcogen.com>
2012-01-06 17:20:35 +0100

    i18n wip

--- lib/generators/bootstrap_forms/install/templates/bootstrap_form_builder.rb
+++ lib/generators/bootstrap_forms/install/templates/bootstrap_form_builder.rb
@@ -5,7 +5,7 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
     if object.errors.full_messages.any?
       content_tag(:div, :class => 'alert-message block-message error') do
         link_to('&times;'.html_safe, '#', :class => 'close') +
-        content_tag(:p, "<strong>Oh snap! You got an error!</strong> Fix the errors below and try again.".html_safe) +
+        content_tag(:p, t('errors.template.header.one', :model => object.class.model_name.human).html_safe) +
         content_tag(:ul) do
           object.errors.full_messages.map do |message|
             content_tag(:li, message)
@@ -16,13 +16,13 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       '' # return empty string
     end
   end
-  
+
   %w(collection_select select check_box email_field file_field number_field password_field phone_field radio_button range_field search_field telephone_field text_area text_field url_field).each do |method_name|
     define_method(method_name) do |name, *args|
       @name = name
       @options = args.extract_options!
       @args = args
-      
+
       clearfix_div do
         label_field + input_div do
           extras { super(name, *(@args << @options)) }
@@ -30,12 +30,12 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       end
     end
   end
-  
+
   def collection_check_boxes(attribute, records, record_id, record_name, *args)
     @name = attribute
     @options = args.extract_options!
     @args = args
-    
+
     clearfix_div do
       label_field + input_div do
         extras do
@@ -43,24 +43,24 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
             records.collect do |record|
               element_id = "#{object_name}_#{attribute}_#{record.send(record_id)}"
               checkbox = check_box_tag("#{object_name}[#{attribute}][]", record.send(record_id), object.send(attribute).include?(record.send(record_id)), :id => element_id)
-          
+
               content_tag(:li) do
                 content_tag(:label) do
                   checkbox + content_tag(:span, record.send(record_name))
                 end
               end
             end.join('').html_safe
-          end 
+          end
         end
       end + hidden_field_tag("#{object_name}[#{attribute}][]")
     end
   end
-  
+
   def collection_radio_buttons(attribute, records, record_id, record_name, *args)
     @name = attribute
     @options = args.extract_options!
     @args = args
-    
+
     clearfix_div do
       label_field + input_div do
         extras do
@@ -68,7 +68,7 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
             records.collect do |record|
               element_id = "#{object_name}_#{attribute}_#{record.send(record_id)}"
               radiobutton = radio_button_tag("#{object_name}[#{attribute}][]", record.send(record_id), object.send(attribute) == record.send(record_id), :id => element_id)
-            
+
               content_tag(:li) do
                 content_tag(:label) do
                   radiobutton + content_tag(:span, record.send(record_name))
@@ -80,12 +80,12 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       end
     end
   end
-  
+
   def uneditable_field(name, *args)
     @name = name
     @options = args.extract_options!
     @args = args
-    
+
     clearfix_div do
       label_field + input_div do
         extras do
@@ -96,32 +96,32 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       end
     end
   end
-  
+
   def submit(name = nil, *args)
     @name = name
     @options = args.extract_options!
     @args = args
-    
+
     @options[:class] = 'btn primary'
-    
+
     content_tag(:div, :class => 'actions') do
       super(name, *(args << @options)) + ' ' + link_to('Cancel', :back, :class => 'btn')
     end
   end
-  
+
   private
   def clearfix_div(&block)
     @options[:error] = object.errors[@name].collect{|e| "#{@options[:label] || @name} #{e}".humanize}.join(', ') unless object.errors[@name].empty?
-    
+
     klasses = ['clearfix']
     klasses << 'error' if @options[:error]
     klasses << 'success' if @options[:success]
     klasses << 'warning' if @options[:warning]
     klass = klasses.join(' ')
-    
+
     content_tag(:div, :class => klass, &block)
   end
-  
+
   def input_div(&block)
     content_tag(:div, :class => 'input') do
       if @options[:append] || @options[:prepend]
@@ -133,12 +133,12 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       end
     end
   end
-  
+
   def label_field(&block)
     required = object.class.validators_on(@name).any? { |v| v.kind_of? ActiveModel::Validations::PresenceValidator }
     label(@name, block_given? ? block : @options[:label], :class => ('required' if required))
   end
-  
+
   %w(help_inline error success warning help_block append prepend).each do |method_name|
     define_method(method_name) do |*args|
       return '' unless value = @options[method_name.to_sym]
@@ -148,7 +148,7 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
       content_tag(:span, value, :class => klass)
     end
   end
-  
+
   def extras(&block)
     [prepend, (yield if block_given?), append, help_inline, error, success, warning, help_block].join('').html_safe
   end
